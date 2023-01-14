dfc5defb09a8a24a367a369a0e67eb6c1037e406
dinh292 <119478119+dinh292@users.noreply.github.com>
2023-01-14 18:06:15 +0700

    change content

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-const h1=document.querySelector('h1 span#world');setInterval(()=>{h1.textContent==='world!'?h1.textContent='Dinh':h1.textContent==='Dinh'&&(h1.textContent='world!')},1000);
+const h1=document.querySelector('h1 span#World');setInterval(()=>{h1.textContent==='World!'?h1.textContent='Dinh':h1.textContent==='Dinh'&&(h1.textContent='World!')},1000);
